dfddf9e303ae7f8eae5797e562b13b3226bea836
Asia Kaplanyan <a.kaplanyan@gmail.com>
2023-01-12 13:46:57 -0800

    add files

--- js/scripts.js
+++ js/scripts.js
@@ -7,19 +7,30 @@ window.onload = function () {
         const value1 = parseInt(document.querySelector("input#value1").value);
         const value2 = parseInt(document.querySelector("input#value2").value);
         const value3 = parseInt(document.querySelector("input#value3").value);
-        console.log(value1, value2, value3);
+        const shipping = document.querySelector("input#shipping").value;
+        const payment = document.querySelector("input#payment").value;
 
-        var result = "";
-        if ((value1 === value2) && (value1 === value3)) {
-            result = "Equilateral";      
-        } else if ((value1 + value2 <= value3) || (value2 + value3 <= value1) || (value3 + value1 <= value2)) {
-            result = "Not a triangle";
-        } else if ((value1 === value2) || (value1 === value3) || (value2 === value3)) {
-            result = "Isosceles";
-        } else if ((value1 !== value2) && (value2 !== value3) && (value1 !== value3)) {
-            result = "Scalene";
+        console.log(value1, value2, value3, shipping, payment);
+
+        let result = 0;
+        if (value1 !== 0) {
+            result = result + value1 * 15;
+            if (value1 > 1) {
+            result = result * 0.9;
+            }
+        } if (value2 !== 0) {
+            result = result + value2 * 30;
+            if (value2 > 1) {
+                result = result - value2 * 30* 0.05;
+            }
+        } if (value3 !== 0) {
+            result = result + value2 * 20;
+            if (value3 > 1) {
+                result = result - value3 *20 * 0.07;
+            }
         }
         console.log(result);
         document.getElementById("output").innerText = result;
+        document.getElementById("address").innerText = shipping;
     });
 }
